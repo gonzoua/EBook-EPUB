2285c87c74edc481a3c94ec4e50e06228f3ae479
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2009-12-06 21:33:00 -0800

    Getting ready to public
    - Add license to files
    - More FB2 element types
    - Parse whole FB2 file structure

--- Container.pm
+++ Container.pm
@@ -1,4 +1,26 @@
-# Copyright (C) 2009 by Oleksandr Tymoshenko. All rights reserved.
+# Copyright (c) 2009 Oleksandr Tymoshenko <gonzo@bluezbox.com>
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
 
 # OEPBS Container format implementation
 # http://www.idpf.org/ocf/ocf1.0/download/ocf10.htm
@@ -22,7 +44,7 @@ sub new
 #
 # Add root file (item in <rootfiles> element)
 #
-sub addRootFile
+sub add_root_file
 {
     my ($self, $path, $mediatype) = @_;
     push @{$self->{root_files}},  {
@@ -34,7 +56,7 @@ sub addRootFile
 #
 # Add content. Recurse if it's directory
 #
-sub addPath
+sub add_path
 {
     my ($self, $from_path, $container_path) = @_;
 
@@ -49,7 +71,7 @@ sub addPath
             $dest .= "/";
         }
 
-        if (!isValidPath($dest)) {
+        if (!is_valid_path($dest)) {
             croak("Bad container path: $dest");
             return;
         }
@@ -67,7 +89,7 @@ sub addPath
         find( { wanted => $file_cb, no_chdir => 1 }, $from_path);
     }
     else {
-        if (!isValidPath($container_path)) {
+        if (!is_valid_path($container_path)) {
             croak("Bad container path: $container_path");
             return;
         }
@@ -83,7 +105,7 @@ sub addPath
 #
 # Check if file name conforms specs
 # TODO: make conformant to spec
-sub isValidPath
+sub is_valid_path
 {
     my $path = shift;
     return if($path =~ /META-INF/);
@@ -94,7 +116,7 @@ sub isValidPath
 # 
 # Generate container.xml  for META-INF directory
 #
-sub writeContainer
+sub write_container
 {
     my ($self, $outname) = @_;
     my $container = new IO::File(">$outname");

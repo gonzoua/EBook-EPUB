a622cfd09f6a104c0ed06dee427edb13d90b580c
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2010-08-17 19:05:44 -0700

    Sync example code with latest development

--- lib/EBook/EPUB.pm
+++ lib/EBook/EPUB.pm
@@ -563,16 +563,30 @@ Version 0.3
     my $uuid = $ug->create_from_name_str(NameSpace_URL, "fb2epub.com");
     $epub->add_identifier("urn:uuid:$uuid");
 
-    # Add package content: stylesheet, font, xhtml
+    # Add package content: stylesheet, font, xhtml and cover
     $epub->copy_stylesheet('/path/to/style.css', 'style.css');
     $epub->copy_file('/path/to/figure1.png', 
         'figure1.png', 'image/png');
     $epub->encrypt_file('/path/to/CharisSILB.ttf', 
         'CharisSILB.ttf', 'application/x-font-ttf');
-    $epub->copy_xhtml('/path/to/page1.xhtml', 'page1.xhtml');
+    my $chapter_id = $epub->copy_xhtml('/path/to/page1.xhtml', 
+        'page1.xhtml');
     $epub->copy_xhtml('/path/to/notes.xhtml', 'notes.xhtml',
         linear => 'no'
     );
+
+    # Add top-level nav-point
+    my $navpoint = $epub->add_navpoint(
+            label       => "Chapter 1",
+            id          => $chapter_id,
+            content     => 'page1.xhtml',
+            play_order  => 1 # should always start with 1
+    );
+
+    # Add cover image
+    # Not actual epub standart but does the trick for iBooks
+    my $cover_id = $epub->copy_image('/path/to/cover.jpg', 'cover.jpg');
+    $epub->add_meta_item('cover', $cover_id);
 
     # Generate resulting ebook
     $epub->pack_zip('/path/to/three_men_in_a_boat.epub');
